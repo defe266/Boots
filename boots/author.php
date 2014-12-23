a84d394af6b74bc6b24be87a51ad3a85854c723d
defe266 <gabriel@gabrielgomez.es>
2014-12-23 11:23:09 +0100

    templates changes

--- boots/author.php
+++ boots/author.php
@@ -38,20 +38,22 @@ get_header(); ?>
 				?>
 
 				<?php toolbox_content_nav( 'nav-above' ); ?>
-
-				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>
-
-					<?php
-						/* Include the Post-Format-specific template for the content.
-						 * If you want to overload this in a child theme then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'content', 'post-excerpt' );
-					?>
-
-				<?php endwhile; ?>
-
+				
+				<div class="row">
+					<?php /* Start the Loop */ ?>
+					<?php while ( have_posts() ) : the_post(); ?>
+	
+						<?php
+							/* Include the Post-Format-specific template for the content.
+							 * If you want to overload this in a child theme then include a file
+							 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+							 */
+							get_template_part( 'content', 'post-excerpt' );
+						?>
+	
+					<?php endwhile; ?>
+				</div>
+				
 				<?php toolbox_content_nav( 'nav-below' ); ?>
 
 			<?php else : ?>
--- boots/searchform.php
+++ boots/searchform.php
@@ -4,10 +4,19 @@
  */
 ?>
 	<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-
+		<!--
 		<div class="input-append">
           <input type="text" class="field span2" size="16" name="s" id="s"/>
           <button id="searchsubmit" name="submit" class="submit btn" type="submit"><?php _e( 'Search', 'wpboots' ); ?></button>
+        </div>
+        -->
+        
+        
+        <div class="input-group">
+          <input type="text" class="form-control field" size="16" name="s" id="s"/>
+          <span class="input-group-btn">
+            <button id="searchsubmit" name="submit" class="submit btn btn-default" type="submit"><?php _e( 'Search', 'wpboots' ); ?></button>
+          </span>
         </div>
         
 	</form>
