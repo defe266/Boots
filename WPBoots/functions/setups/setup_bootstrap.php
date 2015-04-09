bce22367ac4015a5a8c2b168cf7995a052d2f408
defe266 <gabriel@gabrielgomez.es>
2015-04-09 11:43:12 +0200

    Youtube in post responsive autofix

--- WPBoots/functions/setups/setup_bootstrap.php
+++ WPBoots/functions/setups/setup_bootstrap.php
@@ -256,4 +256,16 @@ if ( ! function_exists( 'bootstrap_setup' ) ):
 
 endif;
 
+
+//# add responsive filter to embed videos
+function bootstrap_wrap_oembed( $html ){
+
+  $html = preg_replace( '/(width|height)="\d*"\s/', "", $html ); // Strip width and height #1
+  return'<div class="embed-responsive embed-responsive-16by9">'.$html.'</div>'; // Wrap in div element and return #3 and #4
+  
+}
+
+add_filter( 'embed_oembed_html','bootstrap_wrap_oembed',10,1);
+
+
 ?>
