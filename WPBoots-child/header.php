092cbe3cff703ea363ec29a836ac4eebfb3cedf7
defe266 <gabriel@gabrielgomez.es>
2016-02-18 13:17:14 +0100

    Fix metatitle generation

--- WPBoots-child/header.php
+++ WPBoots-child/header.php
@@ -23,28 +23,7 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0,target-densitydpi=160dpi, user-scalable=no" name="viewport">
-<title><?php
-	/*
-	 * Print the <title> tag based on what is being viewed.
-	 */
-	global $page, $paged;
-
-	wp_title( '|', true, 'right' );
-
-	// Add the blog name.
-	bloginfo( 'name' );
-
-	// Add the blog description for the home/front page.
-	$site_description = get_bloginfo( 'description', 'display' );
-	if ( $site_description && ( is_home() || is_front_page() ) )
-		echo " | $site_description";
-
-	// Add a page number if necessary:
-	if ( $paged >= 2 || $page >= 2 )
-		echo ' | ' . sprintf( __( 'Page %s', 'wpboots' ), max( $paged, $page ) );
-
-	?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11" />
+<title><?php wp_title( '|', true, 'right' ); ?></title><link rel="profile" href="http://gmpg.org/xfn/11" />
 <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
